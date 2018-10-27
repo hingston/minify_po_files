124ec47bd239fc9f15e5b0f385de29908fedd19f
William Hingston <will.hingston@gmail.com>
2018-10-27 14:19:03 +1030

    Working on removing fuzzy

--- minify_po_files.py
+++ minify_po_files.py
@@ -10,32 +10,87 @@ import argparse
 import os
 
 
+def translate_po_files(lang: str, line: str):
+    if "%(" not in line and "</" not in line and line is not "":
+        # print( line)
+        pass
+    else:
+        pass
+
+    """
+    # Instantiates a client
+    translate_client = translate.Client()
+
+    # The text to translate
+    text = u'Hello, world!'
+    # The target language
+    target = 'ru'
+
+    # Translates some text into Russian
+    translation = translate_client.translate(text, target_language=target)
+
+    print(u'Text: {}'.format(text))
+    print(u'Translation: {}'.format(translation['translatedText']))
+    """
+
+
 def get_immediate_subdirectories(directory: str):
     return [name for name in os.listdir(directory)
             if os.path.isdir(os.path.join(directory, name))]
 
 
-def minify_po_files(path: str, encoding: str = "utf8", print_output: bool = False):
+def minify_po_files(path: str, encoding: str = "utf8", print_output: bool = False, translate_now: bool = False):
     langs = get_immediate_subdirectories(path)
+    langs = ["zh_Hans"]
+    langs = ["de"]
     for lang in langs:
         with open(path + lang + "/LC_MESSAGES/django.po", encoding=encoding) as file:
-            lines = []
-            skip = True
-            # Add all lines to a list
-            first = True
+            lines_all = []
+            skip = False
+            fuzzy = False
+            # Add all lines to a list but ignore all fuzzy msgstr
             for line in file:
-                if not line.startswith("#"):
-                    if line.startswith('msgid "'):
-                        if line.startswith('msgid ""') and first:
-                            first = False
+                if line.startswith("#") and "fuzzy" in line:
+                    fuzzy = True
+                if line.startswith("#: ."):
+                    fuzzy = False
+                    skip = False
+
+                if fuzzy:
+                    if not skip:
+                        if line.startswith('msgstr "'):
+                            lines_all.append('msgstr ""')
                             skip = True
                         else:
-                            skip = False
-                    if skip:
-                        if not line.startswith('"'):
-                            lines.append(line.rstrip())
+                            lines_all.append(line)
                     else:
+                        lines_all.append("# skipped")
+                else:
+                    lines_all.append(line)
+
+
+
+        lines = []
+        skip = True
+        fuzzy = False
+        # Add all lines to a list
+        first = True
+        for line in lines_all:
+            if not line.startswith("#"):
+                if line.startswith('msgid "'):
+                    if line.startswith('msgid ""') and first:
+                        first = False
+                        skip = True
+                    else:
+                        skip = False
+                if skip:
+                    if not line.startswith('"'):
                         lines.append(line.rstrip())
+                elif line.startswith('msgstr'):
+                    lines.append(line.rstrip())
+                else:
+                    lines.append(line.rstrip())
+
         lines.pop(0)
         lines.pop(0)
 
@@ -79,11 +134,16 @@ def minify_po_files(path: str, encoding: str = "utf8", print_output: bool = Fals
                     part_line = ''
                     finished_lines.append(line)
             else:
+                print(line)
                 finished_lines.append(line)
 
         if finished_lines[len(finished_lines) - 1].startswith('msgid "'):
             finished_lines.append('msgstr ""')
 
+        for line in finished_lines:
+            pass#print(line)
+
+        exit(11)
         with open(path + lang + "/LC_MESSAGES/django.po", "w", encoding=encoding) as output:
             for line in finished_lines:
                 output.write(line + "\n")
@@ -101,9 +161,12 @@ def minify_po_files(path: str, encoding: str = "utf8", print_output: bool = Fals
         with open(path + lang + "/LC_MESSAGES/django.po", encoding=encoding) as file:
             for line in file:
                 if line.startswith('msgid "'):
+                    line_to_translate = line[7:-2]
                     total = total + 1
 
                 if line.startswith('msgstr ""'):
+                    if translate_now:
+                        translate_po_files(lang, line_to_translate)
                     not_translated = not_translated + 1
 
         if -100 * not_translated / total + 100 == 100:
@@ -140,8 +203,10 @@ if __name__ == '__main__':
     parser.add_argument("path", type=str, help="path of Django locale directory")
     parser.add_argument("-p", "--print", help="print percentage translated for readme.md", action="store_true")
     parser.add_argument("-e", "--encoding", type=str, help="encoding, default='utf8'", default="utf8")
+    parser.add_argument("-t", "--translate", help="translate with Google Translate API", action="store_true")
+
     args = parser.parse_args()
 
     # has to be run twice TODO make it only require once
     minify_po_files(args.path, encoding=args.encoding)
-    minify_po_files(args.path, encoding=args.encoding, print_output=args.print)
+    minify_po_files(args.path, encoding=args.encoding, print_output=args.print, translate_now=args.translate)
